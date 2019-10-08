51892582afbfe55b684df8822326eb4ab38cd380
okmanl <ericgumba@gmail.com>
2019-10-07 22:48:26 -0700

    modified app to serve react files

--- app.js
+++ app.js
@@ -30,7 +30,9 @@ app.use(express.json());
 app.use( cors() )
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
 app.use(express.json({
   type: ['application/json', 'text/plain']
 }))
@@ -53,4 +55,12 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
 module.exports = app;
